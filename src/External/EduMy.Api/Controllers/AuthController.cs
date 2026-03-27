d4de88063d024cb30e7ebb5227796296f90d5662
AtanasG6 <gyulchev666@gmail.com>
2026-03-27 22:57:39 +0200

    add auth controller

---
+++ src/External/EduMy.Api/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+using EduMy.Application.Services.Auth;
+using EduMy.Application.Services.Auth.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduMy.Api.Controllers;
+
+public class AuthController : BaseApiController
+{
+    private readonly IAuthService _authService;
+
+    public AuthController(IAuthService authService)
+    {
+        _authService = authService;
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        => ToActionResult(await _authService.LoginAsync(request));
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+        => ToActionResult(await _authService.RegisterAsync(request));
+}
